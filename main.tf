d685e92f0a44b0135e19ac7c00e5534df4996c16
rajaviakhrrc <61845958+rajaviakhrrc@users.noreply.github.com>
2020-03-06 10:53:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
 
+provider "aws" {
+  region = "ca-central-1"
+}
+
+module "backend"{
+  source = "/home/ubuntu/myproject/module/backend"
+
+  bucket = "${var.bucket}"
+  acl = "${var.acl}"
+  name = "${var.name}"
+  billing_mode = "${var.billing_mode}"
+  hash_key = "${var.hash_key}"
+   region = "${var.region}"
+
+}
