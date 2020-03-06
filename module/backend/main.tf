2d01bba6c4da9aa5ce664d3c0c21bb40f97c5e40
rajaviakhrrc <61845958+rajaviakhrrc@users.noreply.github.com>
2020-03-06 11:00:59 -0500

    Update main.tf

--- module/backend/main.tf
+++ module/backend/main.tf
@@ -1,1 +1,22 @@
 
+provider "aws" {
+version = "~> 2.0"
+region = var.region
+}
+
+
+resource "aws_s3_bucket" "b" {
+bucket = var.bucket
+acl = var.acl
+}
+
+resource "aws_dynamodb_table" "l" {
+    name = var.name
+    billing_mode = var.billing_mode
+    hash_key = var.hash_key
+
+ attribute {
+name = "LockID"
+type = "S"
+ }
+}
