86d0e0a3ec892022b6bc0647d25ddc492b357438
rajaviakhrrc <61845958+rajaviakhrrc@users.noreply.github.com>
2020-03-05 19:59:12 -0500

    Update main.tf

--- module/backend/main.tf
+++ module/backend/main.tf
@@ -1,1 +1,29 @@
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "${var.bucket}"
+  acl    = "public-read"
+versioning {
+  enabled = true
+ }
+lifecycle {
+  pervent_destory = true
+   }
+   server_side_encryption_configuration {
+rule {
+apply_server_side_encryption_by_default {
+sse_algorithm = "AES256"
+   }
+  }
+ }
+}
+
+resource "aws_dynamodb_table" "terraform_locks9" {
+    name = "var.dynamodbname"
+    billing_mode = "${var.billing_mode}"
+    hash_key = "${var.hash_key}"
+
+ attribute {
+name = "LockID"
+type = "S"
+ }
+}
 
