69341bf4ef9a8884ae561a925f088401130004bf
rajaviakhrrc <61845958+rajaviakhrrc@users.noreply.github.com>
2020-03-08 15:10:51 -0400

    Update variable.tf
    created some example, but  still need to modified

--- module/vpc/variable.tf
+++ module/vpc/variable.tf
@@ -1,1 +1,6 @@
-
+variable = "vpc_cidr"{}
+variable = "sub_private_cidr_1"{}
+variablen = "sub_private_cidr_2"{}
+variable = "sub_public_cidr_1"{}
+#why there is know pub 2
+#variable = "sub_public_cidr_2"{}
