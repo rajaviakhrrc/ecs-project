cb0bc11bef4388903aaf252f376bf309d0258431
rajaviakhrrc <61845958+rajaviakhrrc@users.noreply.github.com>
2020-03-08 15:06:37 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ version = "~> 2.0"
 region = "var.region"
 }
 
+locals {
+  environment = lookup(var.workspace_to_environment_map, terraform.workspace, "dev")
+}
+
 module "my_backend"{
   source = "../module/backend"
   
