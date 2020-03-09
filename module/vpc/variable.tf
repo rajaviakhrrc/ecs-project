25095afdd64921e203f9add2d496ee58713861f3
rajaviakhrrc <61845958+rajaviakhrrc@users.noreply.github.com>
2020-03-09 19:29:53 -0400

    Update variable.tf
    some default values are given

--- module/vpc/variable.tf
+++ module/vpc/variable.tf
@@ -1,6 +1,33 @@
-variable = "vpc_cidr"{}
-variable = "sub_private_cidr_1"{}
-variablen = "sub_private_cidr_2"{}
-variable = "sub_public_cidr_1"{}
-#why there is know pub 2
-#variable = "sub_public_cidr_2"{}
+#vpc variable.tf
+
+variable "enviroment"{
+  default = "test"
+}
+variable "vpc_name"{
+  default = "my_test_vpc"
+}
+
+variable "vpc_cidr"{
+ default = "10.0.0.0/16"
+}
+variable "sub_private_name" {
+  default = "my_test_private_sub"
+ 
+}
+variable = "sub_private_cidr_1"{
+ default = "10.0.0.0/24"
+}
+variablen = "sub_private_cidr_2"{
+  default = "10.0.1.0/24"
+}
+variable "sub_public_name"{
+  default = "my_test_public_sub"
+}
+variable = "sub_public_cidr_1"{
+  default = "10.0.10.0/24" 
+ }
+variable = "sub_public_cidr_2"{
+  default = "10.0.11.0/24"
+}
+
+
